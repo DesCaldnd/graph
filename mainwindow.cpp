555de1c8d541187db892c82ee3de9e3b863bed88
DesCaldnd <ivanlindemann7@gmail.com>
2023-11-16 01:49:48 +0300

    Init commit

---
+++ mainwindow.cpp
@@ -0,0 +1,136 @@
+//
+// Created by Des Caldnd on 11/14/2023.
+//
+
+// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
+
+#include "mainwindow.h"
+#include "ui_MainWindow.h"
+#include <expression_process.h>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+        QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->box_x_max, &QDoubleSpinBox::valueChanged, this, &MainWindow::changed_x_max);
+    connect(ui->box_x_min, &QDoubleSpinBox::valueChanged, this, &MainWindow::changed_x_min);
+    connect(ui->box_y_max, &QDoubleSpinBox::valueChanged, this, &MainWindow::changed_y_max);
+    connect(ui->box_y_min, &QDoubleSpinBox::valueChanged, this, &MainWindow::changed_y_min);
+    connect(ui->box_precision, &QDoubleSpinBox::valueChanged, this, &MainWindow::changed_precision);
+    connect(ui->line_expression, &QLineEdit::textEdited, this, &MainWindow::prompt_changed);
+
+    ui->widget_graph->xAxis->setRange(x_min, x_max);
+    ui->widget_graph->yAxis->setRange(y_min, y_max);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    vector_destroy(&expr_vector);
+}
+
+void MainWindow::changed_x_max(double val)
+{
+    if(val > x_min)
+    {
+        x_max = val;
+        ui->widget_graph->xAxis->setRange(x_min, x_max);
+        plot();
+    }
+}
+
+void MainWindow::changed_x_min(double val)
+{
+    if(val < x_max)
+    {
+        x_min = val;
+        ui->widget_graph->xAxis->setRange(x_min, x_max);
+        plot();
+    }
+}
+
+void MainWindow::changed_y_max(double val)
+{
+    if(val > y_min)
+    {
+        y_max = val;
+        ui->widget_graph->yAxis->setRange(y_min, y_max);
+    }
+}
+
+void MainWindow::changed_y_min(double val)
+{
+    if(val < y_max)
+    {
+        y_min = val;
+        ui->widget_graph->yAxis->setRange(y_min, y_max);
+    }
+}
+
+void MainWindow::changed_precision(double val)
+{
+    precision = val;
+    plot();
+}
+
+void MainWindow::plot()
+{
+    expr_vector = expression_to_postfix(prompt.data());
+    if (!vector_is_valid(expr_vector))
+        return;
+
+    prepare_data();
+    for (int i = ui->widget_graph->graphCount() - 1; i >= data.size(); --i)
+    {
+        ui->widget_graph->removeGraph(i);
+    }
+    for (int i = ui->widget_graph->graphCount(); i < data.size(); ++i)
+    {
+        ui->widget_graph->addGraph();
+    }
+
+    for (int i = 0; i < data.size(); ++i)
+    {
+        ui->widget_graph->graph(i)->setData(x_data[i], data[i]);
+    }
+
+    ui->widget_graph->replot();
+}
+
+void MainWindow::prompt_changed(QString string)
+{
+    prompt = string.toStdString();
+    plot();
+}
+
+void MainWindow::prepare_data()
+{
+    double x = x_min;
+    int vec_index = 0;
+    bool need_new = false;
+    data.clear();
+    data.push_back(QVector<double>());
+    x_data.clear();
+    x_data.push_back(QVector<double>());
+
+    while (x < x_max)
+    {
+        double val = calculate_expression(expr_vector, x);
+        if (val != NAN && val != INFINITY && val != -INFINITY)
+        {
+            x_data[vec_index].push_back(x);
+            data[vec_index].push_back(val);
+            need_new = true;
+        } else if (need_new)
+        {
+
+            data.push_back(QVector<double>());
+            x_data.push_back(QVector<double>());
+            need_new = false;
+            ++vec_index;
+        }
+        x += precision;
+    }
+}
---
+++ mainwindow.h
@@ -0,0 +1,50 @@
+//
+// Created by Des Caldnd on 11/14/2023.
+//
+
+#ifndef GRAPH_MAINWINDOW_H
+#define GRAPH_MAINWINDOW_H
+
+#include <QMainWindow>
+#include <structs.h>
+#include <string>
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+    class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+
+    ~MainWindow() override;
+
+public slots:
+
+    void changed_x_max(double val);
+    void changed_x_min(double val);
+    void changed_y_max(double val);
+    void changed_y_min(double val);
+    void changed_precision(double val);
+    void prompt_changed(QString string);
+
+private:
+    vector expr_vector;
+    double x_max = 1, x_min = -1, y_max = 1, y_min = -1, precision = 0.01;
+    std::string prompt;
+    Ui::MainWindow *ui;
+    QVector<QVector<double>> data, x_data;
+
+    void plot();
+    void prepare_data();
+};
+
+
+#endif //GRAPH_MAINWINDOW_H
